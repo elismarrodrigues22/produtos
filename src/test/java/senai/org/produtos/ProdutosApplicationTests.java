72474a57daba2de5433989fe61226ba651337413
elismar <infotec.elismar@gmail.com>
2022-11-21 00:31:59 -0300

    projeto-produtos

---
+++ src/test/java/senai/org/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.org.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
